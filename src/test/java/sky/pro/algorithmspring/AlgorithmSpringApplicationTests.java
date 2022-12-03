2506f0d545f271a80f4f29dd4616c7226d13e658
Ann <niyta@mail.ru>
2022-12-03 13:31:55 +0500

    Initial commit

---
+++ src/test/java/sky/pro/algorithmspring/AlgorithmSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.algorithmspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
